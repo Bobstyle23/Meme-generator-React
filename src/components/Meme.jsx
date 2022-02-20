7ed64fba1ddf35548c426832f0e6c85a1c602152
Bob Pakhriev <boburpakhriev@gmail.com>
2022-02-20 16:07:02 +0900

    Add background color, add box-shadow

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -12,8 +12,8 @@ const Meme = () => {
   return (
     <div className="meme-container">
       <form className="inputs">
-        <input className="field-1" type="text" />
-        <input className="field-2" type="text" />
+        <input className="field-1" type="text" placeholder="Top Text" />
+        <input className="field-2" type="text" placeholder="Bottom Text" />
       </form>
       <button onClick={getMemeImage} className="generate-btn">
         Get a new meme image 🖼
